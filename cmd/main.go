d91e2081b2f2d1f0869e5f05bb194df062e71bd6
Aibekabdi <pigeonthe@gmail.com>
2024-06-29 00:03:37 +0500

    loading config and setupping logger created

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"log/slog"
+	"os"
+	"time-tracker/config"
+)
+
+const (
+	envLocal = "local"
+)
+
+var (
+	errSetupLogger = errors.New("invalid level of application")
+)
+
+func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = setupLogger(conf.EnvLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupLogger(env string) (*slog.Logger, error) {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	default:
+		return nil, errSetupLogger
+	}
+	return log, nil
+}
---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	EnvLevel string `envconfig:"ENV_LEVEL" default:"local"`
+	Server
+}
+
+type Server struct {
+	Address     string        `envconfig:"SERVER_ADDRESS" default:"127.0.0.1:8000"`
+	Timeout     time.Duration `envconfig:"SERVER_TIMEOUT" default:"4s"`
+	IdleTimeout time.Duration `envconfig:"SERVER_IDLE_TIMEOUT" default:"30s"`
+}
+
+type DB struct {
+}
+
+func LoadConfig() (*Config, error) {
+	var config Config
+	if err := envconfig.Process("", &config); err != nil {
+		return nil, fmt.Errorf("unvailable to load config: %s", err.Error())
+	}
+	return &config, nil
+}
